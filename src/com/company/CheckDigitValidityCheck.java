3a4c6a80e7cf8ebeb0c1d522c773e9f3b29249a0
Niklas Appelquist <niklasappelquist@gmail.com>
2021-11-08 14:20:39 +0100

    Remove unused import

--- src/com/company/CheckDigitValidityCheck.java
+++ src/com/company/CheckDigitValidityCheck.java
@@ -1,7 +1,5 @@
 package com.company;
 
-import com.sun.jdi.CharType;
-
 public class CheckDigitValidityCheck implements ValidityCheck {
     private String message = "Has incorrect check number";
     private boolean isValid;
